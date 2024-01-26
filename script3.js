a62bc204f069c42f37a68c368dede712c6e82a8a
sriramulagopi <srgopi1234@gmail.com>
2024-01-26 13:32:32 +0530

    modified1

--- script3.js
+++ script3.js
@@ -36,3 +36,46 @@
 // let arr = [1,2,3,4,5,6,7,8,9,10];
 // let arr1 = arr.filter(e=>e%2===0);
 // console.log(arr1);
+
+/***** Strings ******/
+// let str = "sriramulagopi1234";
+// console.log(str.charCodeAt(1));
+// console.log(str.charCodeAt(str.length-2));
+// console.log(String.fromCharCode(65));
+
+// let str = "sriramulagopi1234";
+// let str1 = str.split('');
+// console.log(str1);
+// let str2 = str1.join(',');
+// console.log(str2);
+
+// let arr = [4,2,9];
+// let arr1 = arr.join('ABC');
+// console.log(arr1);
+
+// console.log("1&2&3&4&5&6".split('&'))
+// let str = "1,2,3,4,5,6;9,0;11,12"
+// let str1 = str.split(/,|;/);
+// console.log(str1);
+
+// let array = [1,2,3,4,5,6,7];
+// for (let i in array){
+//     console.log(typeof i,typeof array[i]);
+//     console.log(i,array[i]);
+// }
+// for (let i of array){
+//     console.log(i ,typeof i);
+// }
+
+// let object = {
+//     Name:"Gopi",
+//     Address:"Khammam",
+//     Street:"BokkalaGadda",
+//     PhoneNo:9848226388
+// }
+// for (let i in object){
+//     console.log(i,object[i]);
+// }
+// for (let i of object){
+//     console.log(i);
+// }
